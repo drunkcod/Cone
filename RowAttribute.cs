2156c804dcd0031e0b3549238e1981f42aea798e
Torbjorn Gyllebring <i.am@drunkencoder.com>
2010-07-22 18:44:56 +0200

    RowTests version first that doesn't look utterly broken inside.

---
+++ RowAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cone
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class RowAttribute : Attribute
+    {
+        public readonly object[] Parameters;
+        public bool Pending;
+        public RowAttribute(params object[] args) { Parameters = args; }
+    }
+}
