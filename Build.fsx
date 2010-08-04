1673fb87218951c31ee9c77a1836bb8347a232eb
drunkcod <i.am@drunkencoder.com>
2010-08-04 16:54:02 +0200

    Buildscript now builds for NUnit 2.5.5 and 2.5.7

--- Build.fsx
+++ Build.fsx
@@ -3,14 +3,14 @@ open System.Diagnostics
 open System.IO
 open Ionic.Zip
 
-let build() =
+let build args =
   let fxPath = System.Runtime.InteropServices.RuntimeEnvironment.GetRuntimeDirectory()  
   let msBuild4 = Path.Combine(fxPath, @"..\v4.0.30319\MSBuild.exe")
   let build =
     Process.Start(
       ProcessStartInfo(
         FileName = msBuild4,
-        Arguments = "Cone.sln /nologo /m /p:Configuration=Release /p:NUnitVersion=2.5.7.10213",
+        Arguments = "Cone.sln /nologo /m /p:Configuration=Release " + args ,
         UseShellExecute = false))
   build.WaitForExit()
   build.ExitCode = 0
@@ -18,13 +18,18 @@ let build() =
 let package() =
     use zip = new ZipFile()
 
-    ["Bin\Cone.dll"; "Bin\Cone.Addin.dll"]
-    |> zip.AddFiles
-
+    zip.AddDirectory("Bin", "Bin") |> ignore
+    zip.RemoveEntry("Bin\Cone.Addin.dll") |> ignore
     zip.AddDirectory("Samples", "Samples") |> ignore
     zip.AddFile("Cone.Samples.sln") |> ignore
     zip.AddFile("Install.txt") |> ignore
     zip.Save(@"Bin\Cone-0.0.1.0.zip")
     true
 
-build() && package()
+Directory.Delete("Build", true)
+Directory.Delete("Bin", true)
+
+build ""
+&& build "/p:NUnitVersion=2.5.5.10112 /t:Cone_Addin:Rebuild"
+&& build "/p:NUnitVersion=2.5.7.10213 /t:Cone_Addin:Rebuild"
+&& package()
