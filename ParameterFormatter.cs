d7863f11bddbc43a85ebbbe2abd5ae68b359b303
drunkcod <i.am@drunkencoder.com>
2010-08-27 15:51:48 +0200

    Handling of array/collection parameters added.

--- ParameterFormatter.cs
+++ ParameterFormatter.cs
@@ -1,7 +1,27 @@
-﻿namespace Cone
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace Cone
 {
     public class ParameterFormatter
     {
-        public string Format(object obj) { return obj.ToString(); }
+        public string Format(object obj) {
+            var collection = obj as IEnumerable;
+            if (collection != null)
+                return FormatCollection(collection);
+            return obj.ToString(); 
+        }
+
+        string FormatCollection(IEnumerable collection) {
+            var result = new StringBuilder("{");
+            var format = " {0}";
+            foreach (var item in collection) {
+                result.AppendFormat(format, item);
+                format = ", {0}";
+            }
+            result.Append(" }");
+            return result.ToString();            
+        }
     }
 }
---
+++ Specs/ParameterFormatterSpec.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Cone
+{
+    [Describe(typeof(ParameterFormatter))]
+    public class ParameterFormatterSpec
+    {
+        ParameterFormatter formatter = new ParameterFormatter();
+
+        public void should_display_array_elements() {
+
+            Verify.That(() => Format(new[] { 1, 2, 3 }) == "{ 1, 2, 3 }");
+        }
+
+        string Format(object obj) { return formatter.Format(obj); }
+    }
+}
