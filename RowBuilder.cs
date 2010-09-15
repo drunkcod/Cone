eabc056a388c1c06acea0773c433b8c0fba652d7
drunkcod <i.am@drunkencoder.com>
2010-09-15 21:32:40 +0200

    De-dupe.

---
+++ RowBuilder.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq.Expressions;
+
+namespace Cone
+{
+    public class RowBuilder<T> : IEnumerable<IRowTestData>
+    {
+        readonly ConeTestNamer testNamer = new ConeTestNamer();
+        readonly List<IRowTestData> rows = new List<IRowTestData>();
+
+        class RowTestData : IRowTestData
+        {
+            readonly MethodInfo method;
+            readonly object[] parameters;
+            string name;
+            bool isPending;
+
+            public RowTestData(MethodInfo method, object[] parameters) {
+                this.method = method;
+                this.parameters = parameters;
+            }
+
+            public string Name  { get { return name ?? method.Name; } }
+
+            public MethodInfo Method { get { return method; } }
+
+            public object[] Parameters { get { return parameters;; } }
+
+            public bool IsPending { get { return isPending; } }
+
+            public RowTestData SetName(string name) {
+                this.name = name;
+                return this;
+            }
+
+            public RowTestData SetPending(bool isPending) {
+                this.isPending = isPending;
+                return this;
+            }
+        }
+
+        public RowBuilder<T> Add(Expression<Action<T>> testCase) {
+            return AddRow(testCase, row => { });
+        }
+
+        public RowBuilder<T> Add(string name, Expression<Action<T>> testCase) {
+            return AddRow(testCase, row => row.SetName(name));
+        }
+
+        public RowBuilder<T> Pending(Expression<Action<T>> pendingTest) {
+            return AddRow(pendingTest, row => row.SetPending(true));
+        }
+
+        RowBuilder<T> AddRow(Expression<Action<T>> expression, Action<RowTestData> withRow) {
+            var row = CreateRow((MethodCallExpression)expression.Body); 
+            withRow(row);
+            rows.Add(row);
+            return this;
+        }
+
+        RowTestData CreateRow(MethodCallExpression call) {
+            var arguments = call.Arguments;
+            var parameters = new object[arguments.Count];
+            for(var i = 0; i != arguments.Count; ++i)
+                parameters[i] = Collect(arguments[i]);
+            return new RowTestData(call.Method, parameters)
+                .SetName(testNamer.NameFor(call.Method, parameters));
+        }
+
+        object Collect(Expression expression) {
+            return ((ConstantExpression)expression).Value;
+        }
+
+        IEnumerator<IRowTestData> IEnumerable<IRowTestData>.GetEnumerator() { return rows.GetEnumerator(); }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() {
+            return rows.GetEnumerator(); 
+        }
+    }
+}
