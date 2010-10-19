e7d25f32299cc90a4c6c219f9c3941028083ccfe
drunkcod <i.am@drunkencoder.com>
2010-10-19 18:07:30 +0200

    Add. Files. Before. Push.

---
+++ Specs/ExpressionExtensionsSpec.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+
+namespace Cone
+{
+    [Describe(typeof(ExpressionExtensions))]
+    public class ExpressionExtensionsSpec
+    {
+        public void ignore_redundant_casts() {
+            var expression = Expression.Constant("Hello World");
+
+            Verify.That(() => Object.ReferenceEquals(expression, expression.CastTo<string>()));
+        }
+    }
+}
