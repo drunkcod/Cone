d8f74f749db9bdcce2af242082dfff67bf4beb4f
drunkcod <i.am@drunkencoder.com>
2010-11-22 17:29:41 +0100

    Moved to own file.

---
+++ ArrayExpressionStringBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Cone
+{
+    class ArrayExpressionStringBuilder<T>
+    {
+        readonly IFormatter<T> formatter;
+
+        public ArrayExpressionStringBuilder(IFormatter<T> formatter) {
+            this.formatter = formatter;
+        }
+
+        public string Format(IEnumerable<T> collection) {
+            var result = new StringBuilder("new[] {");
+            var format = " {0}";
+            foreach (var item in collection) {
+                result.AppendFormat(format, Format(item));
+                format = ", {0}";
+            }
+            result.Append(" }");
+            return result.ToString();
+        }
+
+        string Format(T value){ return formatter.Format(value); }
+    }
+}
--- ParameterFormatter.cs
+++ ParameterFormatter.cs
@@ -5,28 +5,6 @@ using System.Collections.Generic;
 
 namespace Cone
 {
-    class ArrayExpressionStringBuilder<T>
-    {
-        readonly IFormatter<T> formatter;
-
-        public ArrayExpressionStringBuilder(IFormatter<T> formatter) {
-            this.formatter = formatter;
-        }
-
-        public string Format(IEnumerable<T> collection) {
-            var result = new StringBuilder("new[] {");
-            var format = " {0}";
-            foreach (var item in collection) {
-                result.AppendFormat(format, Format(item));
-                format = ", {0}";
-            }
-            result.Append(" }");
-            return result.ToString();
-        }
-
-        string Format(T value){ return formatter.Format(value); }
-    }
-
     public class ParameterFormatter : IFormatter<object>
     {
         public string Format(object obj) {
