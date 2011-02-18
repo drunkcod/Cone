65ff5262cd6b5933a710e4125ebceb2d838417ef
drunkcod <i.am@drunkencoder.com>
2011-02-18 10:07:53 +0100

    TutorialSpec added.

---
+++ Samples/TutorialSpec.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Cone.Samples
+{
+    [Describe(typeof(TutorialSpec))]
+    public class TutorialSpec
+    {
+        [Explicit]
+        public void this_test_should_be_explicit() {          
+            Verify.That(() => false); 
+        }
+    }
+}
