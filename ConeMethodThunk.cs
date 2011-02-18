5ec3a1d4b54f86da2cc822131f1f4c6e12a6f9c2
drunkcod <i.am@drunkencoder.com>
2011-02-18 20:31:26 +0100

    ConeMethodThunk extracted to ease playing around with alternative invocation strategies

--- ConeMethodThunk.cs
+++ ConeMethodThunk.cs
@@ -1,20 +1,40 @@
 ﻿using System.Reflection;
+using System.Reflection.Emit;
+using System;
 
 namespace Cone
 {
+    delegate void Thunk(object target, object[] parameters);
+
+    static class ThunkCompiler 
+    {
+        static Type[] ThunkParameters = new []{ typeof(object), typeof(object[]) };
+
+        public static Thunk CreateThunk(MethodInfo targetMethod) {
+            var thunk = new DynamicMethod(targetMethod.Name + "Thunk", null, ThunkParameters);
+            var parameters = targetMethod.GetParameters();
+            if(targetMethod.IsStatic) {
+            } else {
+            }
+
+            return (Thunk)thunk.CreateDelegate(typeof(Thunk));
+            
+        }
+    }
+
     public class ConeMethodThunk : ICustomAttributeProvider
     {
         readonly MethodInfo method;
         readonly ConeTestNamer namer;
+        readonly Thunk thunk;
 
         public ConeMethodThunk(MethodInfo method, ConeTestNamer namer) {
             this.method = method;
             this.namer = namer;
+            this.thunk = ThunkCompiler.CreateThunk(method);
         }
 
-        public void Invoke(object fixture, object[] parameters) {
-            method.Invoke(fixture, parameters);
-        }
+        public void Invoke(object fixture, object[] parameters) { thunk(fixture, parameters); }
 
         public string NameFor(object[] parameters) {
             return namer.NameFor(method, parameters);
