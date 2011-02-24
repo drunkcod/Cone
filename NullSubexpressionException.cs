fff50a9c8fe71399a0f95d304f7ffa133841f090
drunkcod <i.am@drunkencoder.com>
2011-02-24 18:58:30 +0100

    moved to file.

---
+++ NullSubexpressionException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Cone
+{
+    public class NullSubexpressionException : ArgumentNullException 
+    {
+        readonly Expression expression;
+        readonly Expression nullSubexpression;
+
+        public NullSubexpressionException(Expression expression, Expression nullSubexpression) {
+            this.expression = expression;
+            this.nullSubexpression = nullSubexpression;
+        }
+
+        public Expression Expression { get { return expression; } }
+        public Expression NullSubexpression { get { return nullSubexpression; } }
+    }
+}
