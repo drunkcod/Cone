3297a7365fa84828d3aa04e99b4f0f3b5510c28f
drunkcod <i.am@drunkencoder.com>
2011-03-05 22:18:34 +0100

    .

---
+++ DisplayClassAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Cone
+{
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
+    public class DisplayClassAttribute : Attribute 
+    {
+        readonly Type displayClass;
+
+        public DisplayClassAttribute(Type displayClass, params object[] parameters) {
+            this.displayClass = displayClass;
+        }
+
+        public object DisplayFor(object value) {
+            return displayClass.GetConstructor(new []{ value.GetType() }).Invoke(new[]{ value });
+        }
+    }
+}
