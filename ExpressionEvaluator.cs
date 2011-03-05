2c169e4eaf0f7be806d326786cc8f095cfc02741
drunkcod <i.am@drunkencoder.com>
2011-03-05 15:08:33 +0100

    Moved to own file.

--- ExpressionEvaluator.cs
+++ ExpressionEvaluator.cs
@@ -18,25 +18,6 @@ namespace Cone
         }
     }
 
-    public struct EvaluationResult 
-    {
-        object value;
-        bool isError;
-
-        public static EvaluationResult Failure(Exception e){ return new EvaluationResult { value = e, isError = true }; }
-        public static EvaluationResult Success(object result){ return new EvaluationResult { value = result, isError = false }; }
-
-        public object Value { 
-            get {
-                if(IsError)
-                    throw (Exception)value;
-                return value; 
-            } 
-        }
-        public Exception Error { get { return (Exception)value; } }
-        public bool IsError { get { return isError; } }
-    }
-
     public class ExpressionEvaluator
     {
         static readonly Dictionary<KeyValuePair<Type, Type>, Func<object, object>> converters = new Dictionary<KeyValuePair<Type, Type>,Func<object,object>>();
@@ -249,7 +230,7 @@ namespace Cone
         }
 
         EvaluationResult EvaluateNullSubexpression(Expression expression, Expression context) {
-                return Failure(new NullSubexpressionException(context, expression));
+            return Failure(new NullSubexpressionException(context, expression));
         }
 
         static object GetValue(object target, MemberInfo member) {
