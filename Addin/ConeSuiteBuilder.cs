d6a1d089d8b76ca286d0829800064152e4e8c26f
drunkcod <i.am@drunkencoder.com>
2011-03-06 21:31:34 +0100

    If you got a reasonably named context you can as well use it.

--- Addin/ConeSuiteBuilder.cs
+++ Addin/ConeSuiteBuilder.cs
@@ -7,24 +7,39 @@ namespace Cone.Addin
     {
         public static TestSuite BuildSuite(Type suiteType) {
             var description = DescriptionOf(suiteType);
-            return BuildSuite(suiteType, description.Category, description.SuiteName, description.SuiteType, description.TestName);
+            return BuildSuite(suiteType, description);
         }
 
-        static ConeSuite BuildSuite(Type type, string categories, string parentSuiteName, string suiteType, string name) {
-            var suite = new ConeSuite(type, parentSuiteName, name, suiteType);
+        static ConeSuite BuildSuite(Type type, IFixtureDescription description) {
+            var suite = new ConeSuite(type, description.SuiteName, description.TestName, description.SuiteType);
             var setup = new ConeFixtureSetup(suite, suite.testNamer);
             setup.CollectFixtureMethods(type);
             suite.BindTo(setup.GetFixtureMethods());
             AddNestedContexts(type, suite);
-            suite.AddCategories(categories);
+            suite.AddCategories(description.Category);
             return suite;
         }
 
+        class FixtureDescription : IFixtureDescription
+        {
+            public string Category { get; set; }
+            public string SuiteName { get; set; }
+            public string SuiteType { get; set; }
+            public string TestName { get; set; }
+        }
+
         static void AddNestedContexts(Type suiteType, ConeSuite suite) {
+            var fixtureDescription = new FixtureDescription {
+                SuiteType = "Context",
+                SuiteName = suite.TestName.FullName
+            };
             foreach (var item in suiteType.GetNestedTypes()) {
                 ContextAttribute description;
-                if (item.TryGetAttribute<ContextAttribute, ContextAttribute>(out description))
-                    suite.Add(BuildSuite(item, description.Category, suite.TestName.FullName, "Context", description.Context));
+                if (item.TryGetAttribute<ContextAttribute, ContextAttribute>(out description)) {
+                    fixtureDescription.Category = description.Category;
+                    fixtureDescription.TestName = description.Context;
+                    suite.Add(BuildSuite(item, fixtureDescription));
+                }
             }
         }
 
