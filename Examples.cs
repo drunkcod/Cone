8c6e9eb7dc33dd37e953a86a5a50e4113a5edebc
drunkcod <i.am@drunkencoder.com>
2011-04-04 23:05:49 +0200

    Examples row builder added.

---
+++ Examples.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Cone
+{
+    public class Examples : IEnumerable<IRowTestData>
+    {
+        readonly ConeTestNamer testNamer = new ConeTestNamer();
+        readonly MethodInfo method;
+
+        readonly List<IRowTestData> rows = new List<IRowTestData>();
+
+        protected Examples(MethodInfo method) {
+            this.method = method;
+        }
+
+        IEnumerator<IRowTestData> IEnumerable<IRowTestData>.GetEnumerator() { return rows.GetEnumerator(); }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() { return rows.GetEnumerator(); }
+    
+        protected void AddRow(params object[] parameters) {
+            rows.Add(new RowTestData(method, parameters).SetName(testNamer.NameFor(method, parameters)));
+        }
+    }
+
+    public class Examples<T0> : Examples
+    {
+        public Examples(Action<T0> action): base(action.Method) { }
+        public void Add(T0 arg0) { AddRow(arg0); }
+    }
+
+    public class Examples<T0, T1> : Examples
+    {
+        public Examples(Action<T0, T1> action): base(action.Method) { }
+        public void Add(T0 arg0, T1 arg1) { AddRow(arg0, arg1); }
+    }
+
+    public class Examples<T0, T1, T2> : Examples
+    {
+        public Examples(Action<T0, T1, T2> action): base(action.Method) { }
+        public void Add(T0 arg0, T1 arg1, T2 arg2) { AddRow(arg0, arg1, arg2); }
+    }
+
+    public class Examples<T0, T1, T2, T3> : Examples
+    {
+        public Examples(Action<T0, T1, T2, T3> action): base(action.Method) { }
+        public void Add(T0 arg0, T1 arg1, T2 arg2, T3 arg3) { AddRow(arg0, arg1, arg2, arg3); }
+    }
+
+    public class Examples<T0, T1, T2, T3, T4> : Examples
+    {
+        public Examples(Action<T0, T1, T2, T3, T4> action): base(action.Method) { }
+        public void Add(T0 arg0, T1 arg1, T2 arg2, T3 arg3, T4 arg4) { AddRow(arg0, arg1, arg2, arg3, arg4); }
+    }
+
+    public class Examples<T0, T1, T2, T3, T4, T5> : Examples
+    {
+        public Examples(Action<T0, T1, T2, T3, T4, T5> action): base(action.Method) { }
+        public void Add(T0 arg0, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5) { AddRow(arg0, arg1, arg2, arg3, arg4, arg5); }
+    }
+
+    public class Examples<T0, T1, T2, T3, T4, T5, T6> : Examples
+    {
+        public Examples(Action<T0, T1, T2, T3, T4, T5, T6> action): base(action.Method) { }
+        public void Add(T0 arg0, T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5, T6 arg6) { AddRow(arg0, arg1, arg2, arg3, arg4, arg5, arg6); }
+    }
+}
--- Samples/FreeDeliveryFeature.cs
+++ Samples/FreeDeliveryFeature.cs
@@ -1,37 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Reflection;
-
-namespace Cone.Samples
+﻿namespace Cone.Samples
 {
-    public class Examples : IEnumerable<IRowTestData>
-    {
-        readonly ConeTestNamer testNamer = new ConeTestNamer();
-        readonly MethodInfo method;
-
-        readonly List<IRowTestData> rows = new List<IRowTestData>();
-
-        protected Examples(MethodInfo method) {
-            this.method = method;
-        }
-
-        public IEnumerator<IRowTestData> GetEnumerator() { return rows.GetEnumerator(); }
-
-        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() { return rows.GetEnumerator(); }
-    
-        protected void AddRow(params object[] parameters) {
-            rows.Add(new RowTestData(method, parameters).SetName(testNamer.NameFor(method, parameters)));
-        }
-    }
-
-    public class Examples<TArg0, TArg1, TArg2> : Examples
-    {
-        public Examples(Action<TArg0, TArg1, TArg2> action): base(action.Method) { }
-        public void Add(TArg0 arg0, TArg1 arg1, TArg2 arg2) { AddRow(arg0, arg1, arg2); }
-    }
-
     [Feature("Free Delivery")]
     public class FreeDeliveryFeature
     {
@@ -41,7 +9,8 @@ namespace Cone.Samples
             [DisplayAs("{0,-13} {1,-8} {2}")]
             public void Example(string customerType, int bookCount, [DisplayClass(typeof(BoolDisplay), "Yes!", "No.")]bool freeDelivery) { }
 
-            public Examples Rows() { return new Examples<string, int, bool>(Example) {
+            public Examples Rows() { 
+                return new Examples<string, int, bool>(Example) {
                     { "Regular" , 10, false },
                     {  "VIP"    ,  5, false },
                     {  "VIP"    ,  6, true  }
