c4c13af963b491853b83727ad1022d2cdb31a505
drunkcod <i.am@drunkencoder.com>
2011-04-06 23:05:05 +0200

    ConeFixtureSetup now totally event driven.

--- Source/ConeFixtureSetup.cs
+++ Source/ConeFixtureSetup.cs
@@ -17,97 +17,46 @@ namespace Cone
     public class ConeFixtureSetup
     {
         readonly ConeMethodClassifier classifier;
-        MethodInfo[] methods;
-        ConeMethodClass[] marks;
-        int beforeAllCount, beforeEachCount, afterEachCount, afterEachWithResultCount, afterAllCount, rowSourceCount;
         readonly IConeSuite suite;
         readonly ConeTestNamer testNamer;
+
+        readonly List<MethodInfo> beforeAll = new List<MethodInfo>();
+        readonly List<MethodInfo> beforeEach = new List<MethodInfo>();
+        readonly List<MethodInfo> afterEach = new List<MethodInfo>();
+        readonly List<MethodInfo> afterEachWithResult = new List<MethodInfo>();
+        readonly List<MethodInfo> afterAll = new List<MethodInfo>();
+        readonly List<MethodInfo> rowSources = new List<MethodInfo>(); 
 
         public ConeFixtureSetup(IConeSuite suite, ConeTestNamer testNamer) {
             this.suite = suite;
             this.testNamer = testNamer;
             this.classifier = new ConeMethodClassifier();
             classifier.Test += (_, e) => AddTestMethod(e.Method);
+            classifier.RowTest += (_, e) => AddRowTest(e.Method, e.Rows);
+            classifier.BeforeAll += (_, e) => beforeAll.Add(e.Method);
+            classifier.BeforeEach += (_,e) => beforeEach.Add(e.Method);
+            classifier.AfterEach += (_, e) => afterEach.Add(e.Method);
+            classifier.AfterEachWithResult += (_, e) => afterEachWithResult.Add(e.Method);
+            classifier.AfterAll += (_, e) => afterAll.Add(e.Method);
+            classifier.RowSource += (_, e) => rowSources.Add(e.Method);            
         }
 
         public void CollectFixtureMethods(Type type) {
-            methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
-            marks = new ConeMethodClass[methods.Length];
-            ResetCounts();
-
+            var methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
             for (int i = 0; i != methods.Length; ++i)
-                ClassifyMethod(i);
+                Classify(methods[i]);
         }
 
         public ConeFixtureMethods GetFixtureMethods() {
             var x = new ConeFixtureMethods();
-            x.BeforeAll = new MethodInfo[beforeAllCount];
-            x.BeforeEach = new MethodInfo[beforeEachCount];
-            x.AfterEach = new MethodInfo[afterEachCount];
-            x.AfterEachWithResult = new MethodInfo[afterEachWithResultCount];
-            x.AfterAll = new MethodInfo[afterAllCount];
-            x.RowSource = new MethodInfo[rowSourceCount];
-            
-            ResetCounts();
-
-            for (int i = 0; i != methods.Length; ++i) {
-                var method = methods[i];
-
-                if (MarkedAs(ConeMethodClass.BeforeAll, i))
-                    x.BeforeAll[beforeAllCount++] = method;
-                if (MarkedAs(ConeMethodClass.BeforeEach, i))
-                    x.BeforeEach[beforeEachCount++] = method;
-                if (MarkedAs(ConeMethodClass.AfterEach, i))
-                    x.AfterEach[afterEachCount++] = method;
-                if (MarkedAs(ConeMethodClass.AfterEachWithResult, i))
-                    x.AfterEachWithResult[afterEachWithResultCount++] = method;
-                if (MarkedAs(ConeMethodClass.AfterAll, i))
-                    x.AfterAll[afterAllCount++] = method;
-                if (MarkedAs(ConeMethodClass.RowSource, i))
-                    x.RowSource[rowSourceCount++] = method;
-            }
+            x.BeforeAll = beforeAll.ToArray();
+            x.BeforeEach = beforeEach.ToArray();
+            x.AfterEach = afterEach.ToArray();
+            x.AfterEachWithResult = afterEachWithResult.ToArray();
+            x.AfterAll = afterAll.ToArray();
+            x.RowSource = rowSources.ToArray();
 
             return x;
-        }
-
-        void ClassifyMethod(int index) {            
-            var method = methods[index];
-            var mark = Classify(method);            
-            for(var x = (int)mark; x != 0; x = x & (x - 1)) {
-                switch(x & ~(x - 1)) {
-                    case (int)ConeMethodClass.RowTest:
-                        method.Has<RowAttribute>(rows => AddRowTest(method, rows));
-                        break;
-                    case (int)ConeMethodClass.RowSource:
-                        ++rowSourceCount;
-                        break;
-                    case (int)ConeMethodClass.BeforeAll:
-                        ++beforeAllCount;
-                        break;
-                    case (int)ConeMethodClass.BeforeEach:
-                        ++beforeEachCount;
-                        break;
-                    case (int)ConeMethodClass.AfterEach: 
-                        ++afterEachCount;
-                        break;
-                    case (int)ConeMethodClass.AfterEachWithResult:
-                        ++afterEachWithResultCount;
-                        break;
-                    case (int)ConeMethodClass.AfterAll:
-                        ++afterAllCount;
-                        break;
-                }
-            }            
-
-            marks[index] = mark;
-        }
-
-        void ResetCounts() {
-            beforeAllCount = beforeEachCount = afterEachCount = afterEachWithResultCount = afterAllCount = rowSourceCount = 0;
-        }
-
-        bool MarkedAs(ConeMethodClass mark, int index) {
-            return (marks[index] & mark) != 0;
         }
 
         void AddTestMethod(MethodInfo method) {
--- Source/ConeMethodClassifier.cs
+++ Source/ConeMethodClassifier.cs
@@ -30,6 +30,17 @@ namespace Cone
         public MethodInfo Method { get { return method; } }
     }
 
+    public class RowTestClassEventArgs : MethodClassEventArgs 
+    {
+        readonly RowAttribute[] rows;
+
+        public RowTestClassEventArgs(MethodInfo method, RowAttribute[] rows) : base(method) {
+            this.rows = rows;
+        }
+
+        public RowAttribute[] Rows { get { return rows; } }
+    }
+
     static class EventHandlerExtensions 
     {
         public static void Raise<T>(this EventHandler<T> self, object sender, T e) where T : EventArgs {
@@ -41,34 +52,55 @@ namespace Cone
     public class ConeMethodClassifier 
     {
         public event EventHandler<MethodClassEventArgs> Test;
+        public event EventHandler<RowTestClassEventArgs> RowTest;
+        public event EventHandler<MethodClassEventArgs> RowSource;
+        public event EventHandler<MethodClassEventArgs> BeforeAll;
+        public event EventHandler<MethodClassEventArgs> BeforeEach;
+        public event EventHandler<MethodClassEventArgs> AfterEach;
+        public event EventHandler<MethodClassEventArgs> AfterEachWithResult;
+        public event EventHandler<MethodClassEventArgs> AfterAll;
 
         public ConeMethodClass Classify(MethodInfo method) {
             if(method.DeclaringType == typeof(object))
                 return ConeMethodClass.Unintresting;
             var attributes = method.GetCustomAttributes(true);
 
-            if(attributes.Any(x => x is RowAttribute))
+            if(method.Has<RowAttribute>(rows => RowTest.Raise(this, new RowTestClassEventArgs(method, rows))))
                 return ConeMethodClass.RowTest;
-            
+           
             var parameters = method.GetParameters();
             if(parameters.Length == 0)
                 return ClassifyNiladic(method, attributes);
             else if(parameters.Length == 1)
-                return ClassifyMonadic(attributes, parameters[0]);
+                return ClassifyMonadic(method, attributes, parameters[0]);
             return ConeMethodClass.Unintresting;
         }
 
         ConeMethodClass ClassifyNiladic(MethodInfo method, object[] attributes) {
-            if(typeof(IEnumerable<IRowTestData>).IsAssignableFrom(method.ReturnType))
+            if(typeof(IEnumerable<IRowTestData>).IsAssignableFrom(method.ReturnType)) {
+                RowSource.Raise(this, new MethodClassEventArgs(method));
                 return ConeMethodClass.RowSource;
+            }
 
             var mark = ConeMethodClass.Unintresting;
             for(int i = 0; i != attributes.Length; ++i) {
                 var item = attributes[i];
-                if(item is BeforeAllAttribute) mark |= ConeMethodClass.BeforeAll;
-                if(item is BeforeEachAttribute) mark |= ConeMethodClass.BeforeEach;
-                if(item is AfterEachAttribute) mark |= ConeMethodClass.AfterEach;
-                if(item is AfterAllAttribute) mark |= ConeMethodClass.AfterAll;
+                if(item is BeforeAllAttribute) {
+                    BeforeAll.Raise(this, new MethodClassEventArgs(method));
+                    mark |= ConeMethodClass.BeforeAll;
+                }
+                if(item is BeforeEachAttribute) {
+                    BeforeEach.Raise(this, new MethodClassEventArgs(method));
+                    mark |= ConeMethodClass.BeforeEach;
+                }
+                if(item is AfterEachAttribute) {
+                    AfterEach.Raise(this, new MethodClassEventArgs(method));
+                    mark |= ConeMethodClass.AfterEach;
+                }
+                if(item is AfterAllAttribute) {
+                    AfterAll.Raise(this, new MethodClassEventArgs(method));
+                    mark |= ConeMethodClass.AfterAll;
+                }
             }
             if(mark != ConeMethodClass.Unintresting)
                 return mark;
@@ -77,10 +109,12 @@ namespace Cone
             return ConeMethodClass.Test;
         }
 
-        ConeMethodClass ClassifyMonadic(object[] attributes, ParameterInfo parameter) {
+        ConeMethodClass ClassifyMonadic(MethodInfo method, object[] attributes, ParameterInfo parameter) {
             if(typeof(ITestResult).IsAssignableFrom(parameter.ParameterType) 
-                && attributes.Any(x => x is AfterEachAttribute))
+                && attributes.Any(x => x is AfterEachAttribute)) {
+                AfterEachWithResult.Raise(this, new MethodClassEventArgs(method));
                 return ConeMethodClass.AfterEachWithResult;
+            }
             return ConeMethodClass.Unintresting;
         }
     }
