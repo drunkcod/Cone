36f40905740d8b8b540db3ac4ca2b05ced02a279
drunkcod <i.am@drunkencoder.com>
2011-06-17 10:13:28 +0200

    ILGeneartor stuffage moved to own file.

---
+++ Source/Core/ILGeneratorExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Cone.Core
+{
+    static class ILGeneratorExtensions
+    {
+        public static ILGenerator Ldarg(this ILGenerator il, int index) {
+            il.Emit(OpCodes.Ldarg, index);
+            return il;
+        }
+
+        public static void Ret(this ILGenerator il) {
+            il.Emit(OpCodes.Ret);
+        }
+
+        public static ILGenerator UnboxAsCallable(this ILGenerator il, Type boxedType) {
+            il.Emit(OpCodes.Unbox_Any, boxedType);
+            if(boxedType.IsValueType) {
+                var tmp = il.DeclareLocal(boxedType);
+                il.Emit(OpCodes.Stloc, tmp);
+                il.Emit(OpCodes.Ldloca, tmp);
+            }
+            return il;
+        }
+
+        public static ILGenerator CallAny(this ILGenerator il, MethodInfo method) {
+            il.Emit(method.IsVirtual ? OpCodes.Callvirt : OpCodes.Call, method);
+            return il;
+        }
+
+        public static ILGenerator ToObject(this ILGenerator il, Type topOfStack) {
+            if(topOfStack.IsValueType)
+                il.Emit(OpCodes.Box, topOfStack);
+            return il;
+        }
+    }
+}
--- Source/Core/MemberInfoExtensions.cs
+++ Source/Core/MemberInfoExtensions.cs
@@ -36,37 +36,4 @@ namespace Cone.Core
             return (Func<object,object>)getter.CreateDelegate(typeof(Func<object, object>));
         }
     }
-
-    static class ILGeneratorExtensions
-    {
-        public static ILGenerator Ldarg(this ILGenerator il, int index) {
-            il.Emit(OpCodes.Ldarg, index);
-            return il;
-        }
-
-        public static void Ret(this ILGenerator il) {
-            il.Emit(OpCodes.Ret);
-        }
-
-        public static ILGenerator UnboxAsCallable(this ILGenerator il, Type boxedType) {
-            il.Emit(OpCodes.Unbox_Any, boxedType);
-            if(boxedType.IsValueType) {
-                var tmp = il.DeclareLocal(boxedType);
-                il.Emit(OpCodes.Stloc, tmp);
-                il.Emit(OpCodes.Ldloca, tmp);
-            }
-            return il;
-        }
-
-        public static ILGenerator CallAny(this ILGenerator il, MethodInfo method) {
-            il.Emit(method.IsVirtual ? OpCodes.Callvirt : OpCodes.Call, method);
-            return il;
-        }
-
-        public static ILGenerator ToObject(this ILGenerator il, Type topOfStack) {
-            if(topOfStack.IsValueType)
-                il.Emit(OpCodes.Box, topOfStack);
-            return il;
-        }
-    }
 }
