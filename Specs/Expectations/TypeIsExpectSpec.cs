19c0d074bc84a4abca76d824ae51d43e4070fe71
drunkcod <i.am@drunkencoder.com>
2011-07-13 15:54:53 +0200

    .

--- Specs/Expectations/TypeIsExpectSpec.cs
+++ Specs/Expectations/TypeIsExpectSpec.cs
@@ -16,6 +16,12 @@ namespace Cone.Expectations
             Verify.That(() => typeIs.Check().Success == false);
         }
 
+        public void result_is_actual_object() {
+            var obj = "Hello World";
+            var typeIs = new TypeIsExpect(Body(() => obj is string), obj, typeof(string));
+            Verify.That(() => Object.ReferenceEquals(typeIs.Check().Actual, obj));
+        }
+
         Expression Body(Expression<Func<bool>> body) { return body; }
     }
 }
