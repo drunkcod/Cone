da4fdb5fdf88c9efcc1bfa47883b3e3ca3f860df
drunkcod <i.am@drunkencoder.com>
2011-08-02 15:36:21 +0200

    EventSpy added.

---
+++ Source/Helpers/EventSpy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cone.Helpers
+{
+    public class EventSpy<T> where T : EventArgs
+    {
+        bool hasBeenRaised;
+
+        public bool HasBeenRaised { get { return hasBeenRaised; } }
+
+        public static implicit operator EventHandler<T>(EventSpy<T> self) {
+            return (s, e) => self.hasBeenRaised = true;
+        }
+    }
+}
--- Specs/Expectations/TypeIsExpectSpec.cs
+++ Specs/Expectations/TypeIsExpectSpec.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Linq.Expressions;
 
 namespace Cone.Expectations
@@ -9,7 +6,6 @@ namespace Cone.Expectations
     [Describe(typeof(TypeIsExpect))]
     public class TypeIsExpectSpec
     {
-
         public void null_handling() {
             object obj = null;
             var typeIs = new TypeIsExpect(Body(() => obj is string), null, typeof(string));
