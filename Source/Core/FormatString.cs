a6abcf7cbea902e4e2be73a75d7fe0e8cb407d56
drunkcod <i.am@drunkencoder.com>
2011-12-02 11:12:50 +0100

    FormatString moved to own file.

---
+++ Source/Core/FormatString.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Cone.Core
+{
+    class FormatString
+    {
+        static readonly Regex FormatStringPattern = new Regex(@"\{((?<id>\d)(,.+?)?(:.+?)?)\}", RegexOptions.Compiled);
+        static readonly int IdGroup = FormatStringPattern.GroupNumberFromName("id");
+
+        readonly string format;
+        readonly SortedDictionary<int, string> parts = new SortedDictionary<int,string>();
+
+        public FormatString(string format) 
+        {
+            this.format = format;
+            foreach(Match item in FormatStringPattern.Matches(format))
+                parts.Add(int.Parse(item.Groups[IdGroup].Value), item.Value);
+        }
+
+        public bool HasItemFormat { get { return parts.Count != 0; } }
+
+        public string Format(params object[] args) { return string.Format(ToString(), args); }
+
+        public override string ToString() { return format; }
+    }
+}
