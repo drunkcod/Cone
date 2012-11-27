10ceb831df944bb1540dd64be633aa3e96e2bd73
Torbjörn Gyllebring <i.am@drunkencoder.com>
2012-11-27 09:50:33 +0100

    Moved to own file.

---
+++ Specs/Cone.Specs/RowTestFeature.cs
@@ -0,0 +1,11 @@
+﻿namespace Cone
+{
+	[Feature("row tests")]
+	public class RowTestFeature
+	{
+		public enum MyEnum { Nothing }
+
+		[Row(1)]
+		public void support_int_to_enum_conversion(MyEnum value) { }
+	}
+}
