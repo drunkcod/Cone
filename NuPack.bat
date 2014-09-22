5861c2f9e7b91bdc1b96e64c30ff84c14a1156d1
Torbjörn Gyllebring <i.am@drunkencoder.com>
2014-09-22 22:24:11 +0200

    Bump to 2014.09.22

--- NuPack.bat
+++ NuPack.bat
@@ -1,3 +1,3 @@
 @echo off
-fsi BumpVersion.fsx && fsi Build.fsx && fsi NuPack.fsx && NuGet Pack
+fsi BumpVersion.fsx && fsi Build.fsx && fsi NuPack.fsx && Tools\NuGet Pack
 
--- Source/Version.cs
+++ Source/Version.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2014.9.1.1")]
+[assembly: AssemblyVersion("2014.9.22.1")]
