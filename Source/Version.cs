1a487b99922c4faf525258a4c7e0fedc0dc6c9f8
Torbjörn Gyllebring <i.am@drunkencoder.com>
2015-02-27 09:34:32 +0100

    Bump.

--- Source/Version.cs
+++ Source/Version.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2014.12.17.1")]
+[assembly: AssemblyVersion("2015.2.27.1")]
