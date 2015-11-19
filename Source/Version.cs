46844af58c87f58f627631c1c0196fc4a7b1119c
Tobbe Gyllebring <i.am@drunkencoder.com>
2015-11-19 16:42:14 +0100

    Bump.

--- Source/Version.cs
+++ Source/Version.cs
@@ -2,4 +2,4 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2015.5.19.1")]
+[assembly: AssemblyVersion("2015.11.19.1")]
