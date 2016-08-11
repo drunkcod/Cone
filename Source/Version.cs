f0f199a0c8c21b4274846dd4f522033fb3ad9428
Tobbe Gyllebring <i.am@drunkencoder.com>
2016-08-11 20:22:05 +0200

    2016.8.11

--- Source/Version.cs
+++ Source/Version.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2016.8.10.1")]
-[assembly: AssemblyInformationalVersion("2016.8-beta")]
+[assembly: AssemblyVersion("2016.8.11.2")]
+[assembly: AssemblyInformationalVersion("2016.8.11")]
