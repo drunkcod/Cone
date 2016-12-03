e8100a9780d8d4730a1b373b84bb0f8370b06460
Tobbe Gyllebring <tobbe@drunkcod.com>
2016-12-03 10:39:34 +0100

    15.

---
+++ Source/Conesole/ConesoleResultLogger.cs
@@ -0,0 +1,39 @@
+﻿using Cone.Core;
+using Cone.Runners;
+using System;
+using System.Threading;
+
+namespace Conesole
+{
+
+	class ConesoleResultLogger : ISessionLogger, ISuiteLogger, ITestLogger
+	{
+		public int FailureCount;
+
+		public void WriteInfo(Action<ISessionWriter> output) { }
+
+		public void BeginSession() { }
+
+		public ISuiteLogger BeginSuite(IConeSuite suite) { return this; }
+
+		public void EndSession() { }
+
+		public ITestLogger BeginTest(IConeTest test) { return this; }
+
+		public void EndSuite() { }
+
+		public void Failure(ConeTestFailure failure) {
+			Interlocked.Increment(ref FailureCount);
+		}
+
+		public void Success() { }
+
+		public void Pending(string reason) { }
+
+		public void Skipped() { }
+
+		void ITestLogger.BeginTest() { }
+
+		public void EndTest() { }
+	}
+}
--- Source/Conesole/Program.cs
+++ Source/Conesole/Program.cs
@@ -205,35 +205,4 @@ namespace Conesole
 			return -1;
 		}
 	}
-
-	class ConesoleResultLogger : ISessionLogger, ISuiteLogger, ITestLogger
-	{
-		public int FailureCount;
-
-		public void WriteInfo(Action<ISessionWriter> output) { }
-
-		public void BeginSession() { }
-
-		public ISuiteLogger BeginSuite(IConeSuite suite) { return this; }
-
-		public void EndSession() { }
-
-		public ITestLogger BeginTest(IConeTest test) { return this; }
-
-		public void EndSuite() { }
-
-		public void Failure(ConeTestFailure failure) {
-			Interlocked.Increment(ref FailureCount);
-		}
-
-		public void Success() { }
-
-		public void Pending(string reason) { }
-
-		public void Skipped() { }
-
-		void ITestLogger.BeginTest() { }
-
-		public void EndTest() { }
-	}
 }
