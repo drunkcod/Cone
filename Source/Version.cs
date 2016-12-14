eefa28472475ae5a9e6884a52f0fb01268cc39b1
Tobbe Gyllebring <tobbe@drunkcod.com>
2016-12-14 15:25:00 +0100

    Version bump.

--- Source/Version.cs
+++ Source/Version.cs
@@ -1,6 +1,6 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2016.8.17.1")]
-[assembly: AssemblyInformationalVersion("2016.8.17")]
+[assembly: AssemblyVersion("2016.12.14.1")]
+[assembly: AssemblyInformationalVersion("2016.12.14")]
