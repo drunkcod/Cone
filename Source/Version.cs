bdc968b6ee19a79b49db90323e8a1711435e2688
Tobbe Gyllebring <tobbe@drunkcod.com>
2017-03-06 10:19:49 +0100

    Let's go with that then.

--- Source/Version.cs
+++ Source/Version.cs
@@ -2,5 +2,10 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
+<<<<<<< HEAD
 [assembly: AssemblyVersion("2017.1.17.1")]
 [assembly: AssemblyInformationalVersion("2017.01.13")]
+=======
+[assembly: AssemblyVersion("2017.3.6.1")]
+[assembly: AssemblyInformationalVersion("2017.03.06")]
+>>>>>>> a64d261... Let's go with that then.
