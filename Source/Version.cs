c2311fcb30dd4331f91114c72601a15f1538dd51
Tobbe Gyllebring <tobbe@drunkcod.com>
2017-03-06 10:23:14 +0100

    Fixup.

--- Source/Version.cs
+++ Source/Version.cs
@@ -1,11 +1,6 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-<<<<<<< HEAD
-[assembly: AssemblyVersion("2017.1.17.1")]
-[assembly: AssemblyInformationalVersion("2017.01.13")]
-=======
 [assembly: AssemblyVersion("2017.3.6.1")]
 [assembly: AssemblyInformationalVersion("2017.03.06")]
->>>>>>> a64d261... Let's go with that then.
