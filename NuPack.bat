7026a9085d23d50699b9cb5aba6beadcc0b46201
Tobbe Gyllebring <tobbe@drunkcod.com>
2017-08-01 10:47:35 +0200

    Version bump.

--- NuPack.bat
+++ NuPack.bat
@@ -1,3 +1,2 @@
 @echo off
-fsi BumpVersion.fsx && fsi Build.fsx && fsi NuPack.fsx && Tools\NuGet Pack
-
+fsi BumpVersion.fsx && fsi Build.fsx && fsi NuPack.fsx Cone && Tools\NuGet Pack Cone.nuspec && fsi NuPack.fsx Conesole && Tools\NuGet Pack Conesole.nuspec
--- Source/Version.cs
+++ Source/Version.cs
@@ -1,6 +1,6 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2017.7.26.1")]
+[assembly: AssemblyVersion("2017.7.31.1")]
 [assembly: AssemblyInformationalVersion("2017.07.26")]
