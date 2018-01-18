af2c207b81114ba7e4fbb77333445c48d2803beb
Tobbe Gyllebring <tobbe@nepa.com>
2018-01-18 14:21:40 +0100

    Bump

--- Source/Version.cs
+++ Source/Version.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2017.11.21.0")]
-[assembly: AssemblyInformationalVersion("2017.11.21")]
+[assembly: AssemblyVersion("2018.01.18.0")]
+[assembly: AssemblyInformationalVersion("2018.01.18")]
