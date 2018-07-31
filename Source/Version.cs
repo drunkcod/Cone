802f5840f5b852111f53748e5e4f4101084294ca
Tobbe Gyllebring <tobbe@nepa.com>
2018-07-31 11:01:44 +0200

    Synch bump.

--- Source/Version.cs
+++ Source/Version.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2018.07.10")]
-[assembly: AssemblyInformationalVersion("2018.07.10")]
+[assembly: AssemblyVersion("2018.07.31")]
+[assembly: AssemblyInformationalVersion("2018.07.31")]
