0f5dcd3feea5cb4340955ca30e225277e85ed171
Tobbe Gyllebring <tobbe@nepa.com>
2018-08-02 21:00:08 +0200

    Correctly handle running selected tests.

--- Source/Version.cs
+++ Source/Version.cs
@@ -2,5 +2,5 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2018.07.32")]
-[assembly: AssemblyInformationalVersion("2018.07.32")]
+[assembly: AssemblyVersion("2018.08.02")]
+[assembly: AssemblyInformationalVersion("2018.08.02")]
